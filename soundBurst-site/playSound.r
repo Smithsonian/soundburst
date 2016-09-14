049263a2ca59cd8f135d3a1ff1d93514c833e924
Timur Girgin <tgirgin@blueraster.com>
2016-09-14 17:01:34 -0400

    Sound play/pause working

---
+++ soundBurst-site/playSound.r
@@ -0,0 +1,19 @@
+# playSound = function (input){
+# 
+# }
+# 
+# getPath = function(folderList) {
+#   # names <- get_selected(input$tree, "names")
+#   one <- attr(folderList[[1L]], "ancestry", TRUE)
+#   path <- paste(one, collapse = "/")
+#   path <- paste0(path, "/")
+#   return(path)
+# }
+# 
+# 
+# pauseSound = function () {
+#   paused <<- TRUE
+#   pause(a)
+#   shinyjs::hide(id = "pauseButton", anim = TRUE)
+#   shinyjs::show(id = "playButton", anim = FALSE)
+# }
