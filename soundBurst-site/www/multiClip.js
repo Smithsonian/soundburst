4b86cbdcb27e84f4f017e4ef2de1966888732b0d
Timur Girgin <tgirgin@blueraster.com>
2016-09-23 11:23:35 -0400

    Brush stuff

--- soundBurst-site/www/multiClip.js
+++ soundBurst-site/www/multiClip.js
@@ -3,22 +3,22 @@ $(document).ready(function() {
 
   $("#speciesDropSubmit").click(function() {
     console.log("test");
-    $("#spectrogram_brush").clone(true).prop("id", "test").css({'background-color':'green'}).appendTo("#spectrogram")
-    debugger;
+    var count = $(".completedBrush").length;
+    $("#spectrogram_brush").clone(true).prop("id", count + 1).prop("class", "completedBrush").css({'background-color':'green'}).appendTo("#spectrogram")
   });
-
-  var left = $("#test").css("left");
-  var top = $("#test").css("top");
-  var height = $("#test").css("height");
-  var width = $("#test").css("width");
-  var borderright = $("#test").css("border-right");
-  var borderleft = $("#test").css("border-left");
-
-  $("#spectrogram_brush").css("left", left);
-  $("#spectrogram_brush").css("top", top);
-  $("#spectrogram_brush").css("height", height);
-  $("#spectrogram_brush").css("width", width);
-  $("#spectrogram_brush").css("border-right", borderright);
-  $("#spectrogram_brush").css("border-left", borderleft);
-  $("#test").remove()
+  //
+  // var left = $("#test").css("left");
+  // var top = $("#test").css("top");
+  // var height = $("#test").css("height");
+  // var width = $("#test").css("width");
+  // var borderright = $("#test").css("border-right");
+  // var borderleft = $("#test").css("border-left");
+  //
+  // $("#spectrogram_brush").css("left", left);
+  // $("#spectrogram_brush").css("top", top);
+  // $("#spectrogram_brush").css("height", height);
+  // $("#spectrogram_brush").css("width", width);
+  // $("#spectrogram_brush").css("border-right", borderright);
+  // $("#spectrogram_brush").css("border-left", borderleft);
+  // $("#test").remove()
 });
